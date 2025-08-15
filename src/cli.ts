8531bcb15a878c8535bb701607a8f7916177d061
Carl Rannaberg <carl@pipedrive.com>
2025-08-15 19:20:21 +0300

    Version 0.1.2

--- src/cli.ts
+++ src/cli.ts
@@ -21,7 +21,7 @@ const program = new Command();
 program
   .name('codebase-map')
   .description('A lightweight code indexer that generates comprehensive project maps for LLMs')
-  .version('0.1.1');
+  .version('0.1.2');
 
 // Scan command
 program
